70b75a34750bf8b676d96d7600a86afb5f7e4cfb
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-14 10:38:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Yildiz_Yazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string yildiz = "*";
+
+            Console.WriteLine("yükseklik giriniz:"); 
+            int yükseklik = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < yükseklik; i++) 
+            {
+                Console.WriteLine(yildiz);
+                yildiz += "*";
+            }
+
+
+            /*for (int i = 0; i < yükseklik; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+
+
+            //        Console.Write(yildiz);
+            //    }
+            //    Console.WriteLine('\n');
+
+            } */
+        }
+    }
+}
